da93c8b1621225ce7022d18f53a5186561dea5c3
Yarden.Ingber <yarden.ingber@applitools.com>
2019-11-21 15:22:42 +0200

    improve log message for excluded tests

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/ExcludeFailingTestsListener.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/ExcludeFailingTestsListener.java
@@ -22,8 +22,10 @@ public class ExcludeFailingTestsListener implements IInvokedMethodListener2 {
                 for (Map.Entry<String,String> entry : testParameters.entrySet()) {
                     if (entry.getKey().toLowerCase().equals("dont_run") &&
                             entry.getValue().toLowerCase().contains(iTestResult.getMethod().getMethodName().toLowerCase() + "+" + ((TestSetup) testInstance).mode.toLowerCase())) {
-                        System.out.println("Test: " + iTestResult.getMethod().getMethodName() + " is excluded");
+                        System.out.println("Test: " + iTestResult.getMethod().getMethodName() + "+" + ((TestSetup) testInstance).mode + " is excluded");
                         throw new SkipException("Skipping data set " + testParameters.entrySet().toString());
+                    } else {
+                        System.out.println("Test: " + iTestResult.getMethod().getMethodName() + "+" + ((TestSetup) testInstance).mode + " is running");
                     }
                 }
             }
