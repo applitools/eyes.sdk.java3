9fdb1b9ac0a6653356d44cf1c885cefd4f3e28d3
Yurii <yurii.cherepovskiy@applitools.com>
2019-12-28 23:05:04 +0200

    Add tests TestCounts, TestScrolling, TestVGServerConfigs to run

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/ExcludeFailingTestsListener.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/ExcludeFailingTestsListener.java
@@ -27,11 +27,15 @@ public class ExcludeFailingTestsListener implements IInvokedMethodListener2 {
                 Method method = iTestResult.getMethod().getConstructorOrMethod().getMethod();
                 ((TestSetup) testInstance).beforeMethod(method.getName());
             }
-            else if (testInstance instanceof TestMobileDevices){
+            if (testInstance instanceof TestMobileDevices) {
                 testData = ((TestMobileDevices) testInstance).deviceName
                         + "+" + ((TestMobileDevices) testInstance).platformVersion
                         + "+" + ((TestMobileDevices) testInstance).deviceOrientation.toString()
                         + "+" + ((TestMobileDevices) testInstance).page;
+                handleTests(iTestResult, testParameters, testData);
+            }
+            if (testInstance instanceof TestScrolling) {
+                testData = Boolean.toString(((TestScrolling) testInstance).useEmulation);
                 handleTests(iTestResult, testParameters, testData);
             }
         }
