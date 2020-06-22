10922a57fc7b5fbd3cfc29a18e507fd0c83e3ab4
Tal Goldrat <tal.goldrat@applitools.com>
2020-06-22 10:55:45 +0300

    Removed deprecation from selenium configuration

--- eyes.common.java/src/main/java/com/applitools/eyes/selenium/Configuration.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/selenium/Configuration.java
@@ -1,8 +1,7 @@
 package com.applitools.eyes.selenium;
 
 /**
- * @deprecated  Please use {@link com.applitools.eyes.config.Configuration}
+ * Soon to be deprecated. Please use {@link com.applitools.eyes.config.Configuration}
  */
-@Deprecated
 public class Configuration extends com.applitools.eyes.config.Configuration {
 }
