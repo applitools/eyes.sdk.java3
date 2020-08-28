ccd4e8aafbaaef75624f0d68be04e1c127885e21
Alexander Kachechka <alexander.kachechka@gmail.com>
2020-08-28 10:10:50 +0300

    Updated application capability

--- eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/ScrollRootElementTest.java
+++ eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/ScrollRootElementTest.java
@@ -24,8 +24,7 @@ public class ScrollRootElementTest extends AndroidTestSetup {
     @Override
     protected void setAppCapability() {
         // To run locally use https://applitools.bintray.com/Examples/androidx/1.0.0/app_androidx.apk
-//        capabilities.setCapability("app", "app_androidx");
-        capabilities.setCapability("app", "https://applitools.bintray.com/Examples/androidx/1.1.0/app_androidx.apk");
+        capabilities.setCapability("app", "app_androidx");
     }
 
     @Override
