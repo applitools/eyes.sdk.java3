dd187b2a38ba54cfad6c712adc14617d36866316
Tal Goldrat <tal.goldrat@applitools.com>
2020-10-04 18:53:08 +0300

    Added a test

--- eyes.sdk.core/src/test/java/com/applitools/eyes/visualgrid/model/TestResourceParsing.java
+++ eyes.sdk.core/src/test/java/com/applitools/eyes/visualgrid/model/TestResourceParsing.java
@@ -2,6 +2,8 @@ package com.applitools.eyes.visualgrid.model;
 
 import com.applitools.eyes.Logger;
 import com.applitools.utils.GeneralUtils;
+import com.steadystate.css.parser.SACParserCSS3Constants;
+import com.steadystate.css.parser.Token;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,6 +12,21 @@ import java.net.URI;
 import java.util.Set;
 
 public class TestResourceParsing {
+    @Test
+    public void testCssTokenizer() throws IOException {
+        String cssContent = GeneralUtils.readToEnd(getClass().getResourceAsStream("/clientlibs_all.default.css"));
+        CssTokenizer tokenizer = new CssTokenizer(cssContent);
+        Token token;
+        int urlCounter = 0;
+        while ((token = tokenizer.nextToken()) != null) {
+            if (token.kind == SACParserCSS3Constants.URL || token.kind == SACParserCSS3Constants.URI) {
+                urlCounter++;
+            }
+        }
+
+        // There are exactly 54 imported urls in the resources file
+        Assert.assertEquals(urlCounter, 54);
+    }
 
     @Test
     public void testParseCss() throws IOException {
@@ -18,6 +35,8 @@ public class TestResourceParsing {
                 "https://fvdev-5531.shopci.intdigital.ee.co.uk:18083/etc/designs/ee-web-2015/clientlibs_all.default.css",
                 "text/css", cssContent.getBytes());
         Set<URI> newResources = resource.parse(new Logger(), "");
+
+        // There are exactly 54 imported urls in the resources file but only 36 unique urls
         Assert.assertEquals(newResources.size(), 36);
     }
 }
