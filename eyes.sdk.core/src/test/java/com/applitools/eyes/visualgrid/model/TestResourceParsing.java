ac227b25e6fef7b38461604bc9db1501211d908a
Tal Goldrat <tal.goldrat@applitools.com>
2020-10-06 18:06:16 +0300

    Fixed tests

--- eyes.sdk.core/src/test/java/com/applitools/eyes/visualgrid/model/TestResourceParsing.java
+++ eyes.sdk.core/src/test/java/com/applitools/eyes/visualgrid/model/TestResourceParsing.java
@@ -10,6 +10,7 @@ import org.w3c.dom.css.CSSRuleList;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashSet;
 import java.util.Set;
 
 public class TestResourceParsing {
@@ -19,22 +20,23 @@ public class TestResourceParsing {
         CssTokenizer tokenizer = new CssTokenizer(cssContent);
         Token token;
         int urlCounter = 0;
+        Set<String> uniqueUrls = new HashSet<>();
         while ((token = tokenizer.nextToken()) != null) {
             if (token.kind == SACParserCSS3Constants.URI) {
                 urlCounter++;
+                uniqueUrls.add(token.image);
             }
         }
 
-        // There are exactly 54 imported urls in the resources file
+        // There are exactly 54 imported urls in the resources file and 38 are unique urls
         Assert.assertEquals(urlCounter, 54);
+        Assert.assertEquals(uniqueUrls.size(), 38);
     }
 
     @Test
     public void testParseCss() throws IOException {
         String cssContent = GeneralUtils.readToEnd(getClass().getResourceAsStream("/css_file_with_urls.css"));
-        RGridResource resource = new RGridResource(
-                "https://test.com",
-                "text/css", cssContent.getBytes());
+        RGridResource resource = new RGridResource( "https://test.com", "text/css", cssContent.getBytes());
         Set<URI> newResources = resource.parse(new Logger(), "");
 
         // There are exactly 54 imported urls in the resources file but only 36 unique urls
@@ -44,13 +46,11 @@ public class TestResourceParsing {
     @Test
     public void testParseCssBadFile() throws IOException {
         String cssContent = GeneralUtils.readToEnd(getClass().getResourceAsStream("/clientlibs_all.default.css"));
-        RGridResource resource = new RGridResource(
-                "https://fvdev-5531.shopci.intdigital.ee.co.uk:18083/etc/designs/ee-web-2015/clientlibs_all.default.css",
-                "text/css", cssContent.getBytes());
+        RGridResource resource = new RGridResource( "https://test.com", "text/css", cssContent.getBytes());
         Set<URI> newResources = resource.parse(new Logger(), "");
 
-        // There are exactly 54 imported urls in the resources file but only 36 unique urls
-        Assert.assertEquals(newResources.size(), 36);
+        // There are exactly 54 imported urls in the resources file but only 38 unique urls
+        Assert.assertEquals(newResources.size(), 38);
     }
 
     @Test
