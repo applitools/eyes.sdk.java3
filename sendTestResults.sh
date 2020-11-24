f10419bfad42c7e196308f37871927bf32a34787
Tal Goldrat <tal.goldrat@applitools.com>
2020-11-24 16:28:46 +0200

    Fixed

--- sendTestResults.sh
+++ sendTestResults.sh
@@ -2,7 +2,6 @@
 
 send_test_results() {
   echo "Json to send:"
-  jq -s '.[0].results = [.[].results[]] | .[0]' "$1"*.json
   jq -s '.[0].results = [.[].results[]] | .[0]' "$1"*.json |
     curl -sS \
       -X POST \
@@ -13,7 +12,6 @@ send_test_results() {
 cd "$TRAVIS_BUILD_DIR/report" || exit
 
 groups=$(for file in *.json; do echo "${file%_*.json}"; done | sort -u)
-echo "Groups: $groups"
 for group in $groups; do
   send_test_results "$group"
 done
