4367ae82eed27dd523c569a78ac4ca3a64bda184
Tal Goldrat <tal.goldrat@applitools.com>
2020-11-30 10:55:51 +0200

    Rfe trello 2189 add batchclose api

--- eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/BatchClose.java
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/BatchClose.java
@@ -1,6 +1,7 @@
 package com.applitools.eyes.fluent;
 
 import com.applitools.eyes.Logger;
+import com.applitools.eyes.ProxySettings;
 import com.applitools.utils.ArgumentGuard;
 import com.applitools.utils.GeneralUtils;
 
@@ -9,6 +10,8 @@ import java.util.List;
 public class BatchClose {
     protected final Logger logger;
     protected String serverUrl;
+    protected String apiKey;
+    protected ProxySettings proxySettings;
 
     public BatchClose() {
         this(new Logger());
@@ -24,9 +27,19 @@ public class BatchClose {
         return this;
     }
 
+    public BatchClose setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+        return this;
+    }
+
+    public BatchClose setProxy(ProxySettings proxySettings) {
+        this.proxySettings = proxySettings;
+        return this;
+    }
+
     public EnabledBatchClose setBatchId(List<String> batchIds) {
         ArgumentGuard.notNull(batchIds, "batchIds");
         ArgumentGuard.notContainsNull(batchIds, "batchIds");
-        return new EnabledBatchClose(logger, serverUrl, batchIds);
+        return new EnabledBatchClose(logger, serverUrl, batchIds).setApiKey(apiKey).setProxy(proxySettings);
     }
 }
