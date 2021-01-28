566c504833737dce7c52a5503edece91bed04cc5
Tal Goldrat <tal.goldrat@applitools.com>
2021-01-28 13:52:52 +0200

    Bugfix trello 2436 regions in screenshot

--- eyes.selenium.common/src/main/java/com/applitools/eyes/selenium/fluent/SimpleRegionByElement.java
+++ eyes.selenium.common/src/main/java/com/applitools/eyes/selenium/fluent/SimpleRegionByElement.java
@@ -2,20 +2,23 @@ package com.applitools.eyes.selenium.fluent;
 
 import com.applitools.eyes.*;
 import com.applitools.eyes.fluent.GetSimpleRegion;
+import com.applitools.eyes.selenium.EyesDriverUtils;
+import com.applitools.eyes.selenium.wrappers.EyesWebDriver;
 import com.applitools.eyes.serializers.WebElementSerializer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SimpleRegionByElement implements GetSimpleRegion, IGetSeleniumRegion {
+public class SimpleRegionByElement implements GetSimpleRegion, IGetSeleniumRegion, ImplicitInitiation {
 
+    @JsonIgnore
+    private EyesWebDriver driver;
     @JsonSerialize(using = WebElementSerializer.class)
     protected final WebElement element;
 
@@ -24,11 +27,14 @@ public class SimpleRegionByElement implements GetSimpleRegion, IGetSeleniumRegio
     }
 
     @Override
-    public List<Region> getRegions(EyesScreenshot screenshot) {
-        Point locationAsPoint = element.getLocation();
-        Dimension size = element.getSize();
+    public void init(Logger logger, EyesWebDriver driver) {
+        this.driver = driver;
+    }
 
-        Location adjustedLocation = new Location(locationAsPoint.getX(), locationAsPoint.getY());
+    @Override
+    public List<Region> getRegions(EyesScreenshot screenshot) {
+        Rectangle rectangle = EyesDriverUtils.getVisibleElementRect(element, driver);
+        Location adjustedLocation = new Location(rectangle.x, rectangle.y);
         if (screenshot != null) {
             // Element's coordinates are context relative, so we need to convert them first.
             adjustedLocation = screenshot.convertLocation(adjustedLocation,
@@ -36,7 +42,7 @@ public class SimpleRegionByElement implements GetSimpleRegion, IGetSeleniumRegio
         }
 
         List<Region> value = new ArrayList<>();
-        value.add(new Region(adjustedLocation, new RectangleSize(size.getWidth(), size.getHeight()),
+        value.add(new Region(adjustedLocation, new RectangleSize(rectangle.width, rectangle.height),
                 CoordinatesType.SCREENSHOT_AS_IS));
 
         return value;
