eaea2f0f34287f0ad9502350cb9dfb35622a9744
artem <artem.borodavka@applitools.com>
2021-08-07 17:13:40 +0300

    change eg_config branch to master

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: 'eyes_selenium_java',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_java/java/emitter.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js',
   overrides: [
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_java/java/overrides.js',
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_java/eg.overrides.js'
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/overrides.js',
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/eg.overrides.js'
   ],
-  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_java/java/template.hbs',
-  tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_java/coverage-tests.js',
+  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/template.hbs',
+  tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/coverage-tests.js',
   ext: '.java',
   outPath: './src/test/java/coverage/generic',
 }
