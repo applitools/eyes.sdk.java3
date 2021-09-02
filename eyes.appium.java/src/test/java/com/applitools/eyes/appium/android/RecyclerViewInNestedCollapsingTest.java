94eb14e0ad7dde9ed9eb03c1e70b5b6c6099567c
alexander-kachechka <alexander.kachechka@applitools.com>
2021-09-02 19:55:26 +0300

    Updated test
    * Added scroll action before click the element

--- eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/RecyclerViewInNestedCollapsingTest.java
+++ eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/RecyclerViewInNestedCollapsingTest.java
@@ -2,12 +2,23 @@ package com.applitools.eyes.appium.android;
 
 import com.applitools.eyes.appium.Target;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class RecyclerViewInNestedCollapsingTest extends AndroidTestSetup {
 
     @Test
     public void testScrollRootElement() throws InterruptedException {
+        TouchAction scrollAction = new TouchAction(driver);
+        scrollAction.press(new PointOption().withCoordinates(5, 1300)).waitAction(new WaitOptions().withDuration(Duration.ofMillis(1500)));
+        scrollAction.moveTo(new PointOption().withCoordinates(5, 200));
+        scrollAction.cancel();
+        driver.performTouchAction(scrollAction);
+
         eyes.open(driver, getApplicationName(), "Check RecyclerView inside NestedScrollView and Collapsing layout");
 
         driver.findElementById("btn_recycler_view_nested_collapsing").click();
