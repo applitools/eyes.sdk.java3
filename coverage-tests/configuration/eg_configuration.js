6e98090b6d68e8795897977cd0030e6182b13d8f
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-09-30 20:53:52 +0300

    Set link to test emit

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "eyes_selenium_java",
-  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js",
+  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_old_VG_version/java/emitter.js",
   overrides: [
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/overrides.js",
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/eg.overrides.js"
