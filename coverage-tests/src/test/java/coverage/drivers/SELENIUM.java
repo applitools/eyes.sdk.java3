95946ae1d184dd052554f4ea547054a50436ec99
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-12-30 22:42:31 +0200

    Return bnack Sauce url string

--- coverage-tests/src/test/java/coverage/drivers/SELENIUM.java
+++ coverage-tests/src/test/java/coverage/drivers/SELENIUM.java
@@ -1,8 +1,7 @@
 package coverage.drivers;
 
 public enum SELENIUM {
-    SAUCE("https://" + System.getenv("SAUCE_USERNAME") +":" + System.getenv("SAUCE_ACCESS_KEY") +
-            "@ondemand.saucelabs.com:443/wd/hub"),
+    SAUCE("https://ondemand.saucelabs.com:443/wd/hub"),
     CHROME_LOCAL("http://localhost:4444/wd/hub"),
     FIREFOX_LOCAL("http://localhost:4445/wd/hub");
 
