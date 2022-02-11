fcb8be97c6074ba1f3b8af6b88ba41f0c1972e24
artem <artem.borodavka@applitools.com>
2022-02-11 13:54:19 +0200

    update coverage generated tests eg configuration

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: "eyes_selenium_java",
-  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js",
+  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/emitter.js",
   overrides: [
-    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/overrides.js",
+    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js",
     "./configuration/java_overrides_universal"
   ],
   template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/template.hbs",
-  tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/coverage-tests.js",
+  tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js",
   ext: ".java",
   outPath: "./src/test/java/coverage/generic",
 };
