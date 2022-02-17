6ead6c3987b4ca3ca31786168df12de9ada2e18a
Serhii Khalymon <sergiykhalimon@gmail.com>
2022-02-17 19:09:08 +0200

    Add USDK backend for appium

---
+++ eyes.appium.java/src/main/java/com/applitools/eyes/appium/TRegionMapper.java
@@ -0,0 +1,39 @@
+package com.applitools.eyes.appium;
+
+import com.applitools.ICheckSettings;
+import com.applitools.eyes.Region;
+import com.applitools.eyes.selenium.universal.dto.TRegion;
+import com.applitools.eyes.selenium.universal.mapper.ElementRegionMapper;
+import com.applitools.eyes.selenium.universal.mapper.RectangleRegionMapper;
+import com.applitools.eyes.selenium.universal.mapper.SelectorRegionMapper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TRegionMapper extends com.applitools.eyes.selenium.universal.mapper.TRegionMapper {
+
+    public static TRegion toTRegionFromCheckSettings(ICheckSettings checkSettings) {
+        if (!(checkSettings instanceof AppiumCheckSettings)) {
+            return null;
+        }
+
+        AppiumCheckSettings appiumCheckSettings = (AppiumCheckSettings) checkSettings;
+        By by = appiumCheckSettings.getTargetSelector();
+
+        if (by != null) {
+            return SelectorRegionMapper.toSelectorRegionDto(by);
+        }
+
+        WebElement element = appiumCheckSettings.getTargetElement();
+        if (element != null) {
+            return ElementRegionMapper.toElementRegionDto(element);
+        }
+
+        Region region = appiumCheckSettings.getTargetRegion();
+
+        if (region != null) {
+            return RectangleRegionMapper.toRectangleRegionDto(region);
+        }
+
+        return null;
+    }
+}
