29302768085caa9b419fae66a8b8b1f4d54be95e
alexander-kachechka <alexander.kachechka@applitools.com>
2022-03-09 17:28:13 +0300

    Clear verticalScrollGap on cleanupCachedData

--- eyes.appium.java/src/main/java/com/applitools/eyes/appium/AppiumScrollPositionProvider.java
+++ eyes.appium.java/src/main/java/com/applitools/eyes/appium/AppiumScrollPositionProvider.java
@@ -226,6 +226,7 @@ public abstract class AppiumScrollPositionProvider implements ScrollPositionProv
         this.cachedScrollableView = null;
         this.cachedScrollableViewLocation = null;
         this.cachedScrollableViewRegion = null;
+        this.isVerticalScrollGapSet = false;
     }
 
     public void setScrollRootElement(WebElement scrollRootElement) {
