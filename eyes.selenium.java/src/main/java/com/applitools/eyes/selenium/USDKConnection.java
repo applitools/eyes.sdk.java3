399e079f767690735ed380b55332f8b2c6ea22ba
KananAli96 <kanan.alizada@applitools.com>
2022-03-15 15:45:58 +0400

    remove unnecessary logs

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/USDKConnection.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/USDKConnection.java
@@ -67,7 +67,6 @@ public class USDKConnection {
 
                 @Override
                 public void onTextFrame(String payload, boolean finalFragment, int rsv) {
-                  System.out.println("RESPONSE: " + payload);
                   if (payload.contains("Core.makeManager") || payload.contains("EyesManager.openEyes")) {
                     try {
                       ResponseDto<Reference> referenceResponseDto = objectMapper.readValue(payload, new TypeReference<ResponseDto<Reference>>() {});
@@ -173,7 +172,6 @@ public class USDKConnection {
     SyncTaskListener<ResponseDto<?>> syncTaskListener = new SyncTaskListener<>(logger, request.getKey());
     map.put(request.getKey(), syncTaskListener);
 
-    System.out.println("REQUEST: " + objectMapper.writeValueAsString(request));
     webSocket.sendTextFrame(objectMapper.writeValueAsString(request));
     return syncTaskListener;
   }
