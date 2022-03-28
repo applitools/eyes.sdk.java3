d2655c9a088b00decc020dbcde07a985bdf0d8d8
KananAli96 <kanan.alizada@applitools.com>
2022-03-28 09:32:26 +0400

    get Url from TracedCommandExecutor for selenium 4

--- eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/universal/mapper/DriverMapper.java
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/universal/mapper/DriverMapper.java
@@ -1,6 +1,6 @@
 package com.applitools.eyes.selenium.universal.mapper;
 
-import java.net.URL;
+import java.lang.reflect.Field;
 
 import com.applitools.eyes.selenium.universal.dto.DriverDto;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +11,9 @@ import org.openqa.selenium.remote.RemoteWebDriver;
  * driver mapper
  */
 public class DriverMapper {
+  private static final String EXECUTOR = "executor";
+  private static final String DELEGATE = "delegate";
+  private static final String REMOTE_SERVER = "remoteServer";
 
   public static DriverDto toDriverDto(WebDriver driver) {
     if (driver == null) {
@@ -18,12 +21,42 @@ public class DriverMapper {
     }
 
     RemoteWebDriver remoteDriver = (RemoteWebDriver) driver;
-    URL url = ((HttpCommandExecutor) (remoteDriver.getCommandExecutor())).getAddressOfRemoteServer();
+
     DriverDto driverDto = new DriverDto();
     driverDto.setSessionId(remoteDriver.getSessionId().toString());
-    driverDto.setServerUrl(url.toString());
+    driverDto.setServerUrl(getRemoteServerUrl(remoteDriver));
     driverDto.setCapabilities(remoteDriver.getCapabilities().asMap());
+
     return driverDto;
+  }
+
+
+  private static String getRemoteServerUrl(RemoteWebDriver remoteWebDriver) {
+    String url = null;
+    if (remoteWebDriver.getCommandExecutor() instanceof HttpCommandExecutor) {
+      url = ((HttpCommandExecutor) (remoteWebDriver.getCommandExecutor())).getAddressOfRemoteServer().toString();
+    } else {
+      try {
+        final Class<?> remoteDriverClass = remoteWebDriver.getClass();
+        final Field executorField = remoteDriverClass.getDeclaredField(EXECUTOR);
+        executorField.setAccessible(true);
+        Object tracedCommandExecutor = executorField.get(remoteWebDriver); // TracedCommandExecutor
+
+        final Class<?> tracedCommandExecutorClass = tracedCommandExecutor.getClass();
+        final Field delegateField = tracedCommandExecutorClass.getDeclaredField(DELEGATE); // HttpCommandExecutor
+        delegateField.setAccessible(true);
+        Object httpCommandExecutor = delegateField.get(tracedCommandExecutor);
+
+        final Class<?> httpCommandExecutorClass = httpCommandExecutor.getClass();
+        final Field remoteServerField = httpCommandExecutorClass.getDeclaredField(REMOTE_SERVER);
+        remoteServerField.setAccessible(true);
+        Object remoteServer = remoteServerField.get(httpCommandExecutor);
+        url = remoteServer.toString();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return url;
   }
 
 }
