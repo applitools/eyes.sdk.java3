a8a6e8bc27d17ba67e79d57a4c249b7d048376d2
artem <artem.borodavka@applitools.com>
2022-05-26 13:55:10 +0300

    update EG var add shell script to run generic tests update travis yml to use shell script to run the generic tests

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "eyes_selenium_java",
-  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/emitter.js",
+  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/qa/eg_update/java/emitter.js",
   overrides: [
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js",
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/overrides.js"
