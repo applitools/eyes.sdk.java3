e2813310d2d3e246ca35dbb3753aa6d1c60c9984
KananAli96 <kanan.alizada@applitools.com>
2022-06-10 10:12:51 +0400

    write automated tests for applitools_universal_path

---
+++ eyes-universal-core/src/test/java/com/applitools/eyes/selenium/universal/server/UniversalSdkNativeLoaderTests.java
@@ -0,0 +1,24 @@
+package com.applitools.eyes.selenium.universal.server;
+
+import com.applitools.utils.GeneralUtils;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class UniversalSdkNativeLoaderTests {
+
+    @Test
+    public void testUniversalSdkPath() {
+        try (MockedStatic<GeneralUtils> utilities = Mockito.mockStatic(GeneralUtils.class)) {
+            utilities.when(() -> GeneralUtils.getEnvString("APPLITOOLS_UNIVERSAL_PATH"))
+                    .thenReturn("path");
+            utilities.when(() -> GeneralUtils.getPropertyString("os.name")).thenReturn("macos");
+            UniversalSdkNativeLoader.start();
+            assertEquals("21077", UniversalSdkNativeLoader.getPort());
+            assertEquals("path", GeneralUtils.getEnvString("APPLITOOLS_UNIVERSAL_PATH"));
+        }
+    }
+
+}
