26ff24da1ccade9ac7374ad0c4a6a990fd54b4e5
artem <artem.borodavka@applitools.com>
2022-09-09 14:34:45 +0300

    update firefox driver version

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -3,7 +3,7 @@ module.exports = {
   emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/emitter.js",
   overrides: [
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js",
-    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/overrides.js"
+    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/0a4e7d35e31ffb2536fc7dc89f754ee3db34df6a/java/overrides.js"
   ],
   template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/template.hbs",
   tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js",
