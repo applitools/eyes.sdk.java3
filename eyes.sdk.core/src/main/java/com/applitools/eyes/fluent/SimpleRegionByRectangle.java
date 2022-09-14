d42175769e5aaf6da26ec74c4e8c3a1e66a5dccd
IdosApplitools <ido.elmaleh@applitools.com>
2022-09-14 15:06:16 +0300

    replace Borders with Padding class

--- eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/SimpleRegionByRectangle.java
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/SimpleRegionByRectangle.java
@@ -1,6 +1,7 @@
 package com.applitools.eyes.fluent;
 
 import com.applitools.eyes.EyesScreenshot;
+import com.applitools.eyes.Padding;
 import com.applitools.eyes.Region;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,9 +11,16 @@ import java.util.List;
 
 public class SimpleRegionByRectangle implements GetSimpleRegion {
     private final Region region;
+    private final Padding padding;
 
     public SimpleRegionByRectangle(Region region) {
         this.region = region;
+        this.padding = new Padding();
+    }
+
+    public SimpleRegionByRectangle(Region region, Padding padding) {
+        this.region = region;
+        this.padding = padding;
     }
 
     @JsonProperty("region")
@@ -23,7 +31,10 @@ public class SimpleRegionByRectangle implements GetSimpleRegion {
     @Override
     public List<Region> getRegions( EyesScreenshot screenshot) {
         List<Region> value = new ArrayList<>();
+        region.addPadding(padding);
         value.add(this.region);
         return value;
     }
+
+    public Padding getPadding() { return this.padding; }
 }
