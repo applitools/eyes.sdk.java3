3354cb2aeba7abfd7d797a5ed5f190f977a7f7c5
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-02 15:56:59 +0300

    update coverage-tests emitter + override branch ref

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: "eyes_selenium_java",
-  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/emitter.js",
+  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_webview_support/java/emitter.js",
   overrides: [
     "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js",
-    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/overrides.js"
+    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_webview_support/java/overrides.js"
   ],
   template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/template.hbs",
   tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js",
