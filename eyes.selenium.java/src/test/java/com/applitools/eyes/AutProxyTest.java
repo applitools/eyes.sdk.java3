985e5f6fcdf71865b97a70f39ccde5838e204882
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-03 18:57:26 +0300

    update unit tests + update e2e test

--- eyes.selenium.java/src/test/java/com/applitools/eyes/AutProxyTest.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/AutProxyTest.java
@@ -1,6 +1,7 @@
 package com.applitools.eyes;
 
 import com.applitools.eyes.config.Configuration;
+import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.visualgrid.services.RunnerOptions;
 import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import org.testng.Assert;
@@ -8,17 +9,50 @@ import org.testng.annotations.Test;
 
 public class AutProxyTest {
 
+
+    @Test
+    public void shouldNotBeNullWhenSetInVisualGridRunnerProxy() {
+        ProxySettings proxySettings = new ProxySettings("http://127.0.0.1", 8080);
+        VisualGridRunner visualGridRunner = new VisualGridRunner();
+        visualGridRunner.setProxy(proxySettings);
+
+        Assert.assertNotNull(visualGridRunner.getProxy());
+        Assert.assertNotNull(visualGridRunner.getAutProxy());
+
+        Eyes eyes = new Eyes(visualGridRunner);
+
+        Assert.assertNotNull(eyes.getConfiguration().getAutProxy());
+        Assert.assertNotNull(eyes.getConfiguration().getProxy());
+    }
+
+    @Test
+    public void shouldBeNullWhenNotSet() {
+        VisualGridRunner visualGridRunner = new VisualGridRunner();
+
+        Assert.assertNull(visualGridRunner.getProxy());
+        Assert.assertNull(visualGridRunner.getAutProxy());
+
+        Eyes eyes = new Eyes(visualGridRunner);
+
+        Assert.assertNull(eyes.getConfiguration().getAutProxy());
+        Assert.assertNull(eyes.getConfiguration().getProxy());
+    }
+
     @Test
     public void shouldBeNullWhenNotAssignedInRunnerOptions() {
         RunnerOptions runnerOptions = new RunnerOptions();
-        Assert.assertNull(runnerOptions.getAutProxyDomains());
         Assert.assertNull(runnerOptions.getAutProxy());
-        Assert.assertNull(runnerOptions.getAutProxyMode());
+        Assert.assertNull(runnerOptions.getProxy());
 
         ProxySettings proxySettings = new ProxySettings("http://127.0.0.1", 8080);
         runnerOptions = new RunnerOptions().autProxy(proxySettings);
-        Assert.assertNull(runnerOptions.getAutProxyDomains());
-        Assert.assertNull(runnerOptions.getAutProxyMode());
+        Assert.assertNull(runnerOptions.getProxy());
+        Assert.assertNull(runnerOptions.getAutProxy().getDomains());
+        Assert.assertNull(runnerOptions.getAutProxy().getAutProxyMode());
+
+        runnerOptions = new RunnerOptions().proxy(proxySettings);
+        Assert.assertNotNull(runnerOptions.getProxy());
+        Assert.assertNull(runnerOptions.getAutProxy());
     }
 
     @Test
@@ -173,8 +207,39 @@ public class AutProxyTest {
     public void testAutProxyFromConfiguration() {
         Configuration configuration = new Configuration();
         AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080));
-        configuration.setAutProxy(autProxySettings);
 
+        configuration.setAutProxy(autProxySettings);
+        Assert.assertNull(configuration.getProxy());
         Assert.assertEquals(configuration.getAutProxy(), autProxySettings);
+
+        configuration = new Configuration();
+        ProxySettings proxySettings = new ProxySettings("http://127.0.0.1", 8080);
+
+        configuration.setProxy(proxySettings);
+        Assert.assertNull(configuration.getAutProxy());
+        Assert.assertEquals(configuration.getProxy(), proxySettings);
+    }
+
+    @Test
+    public void shouldAssignConfigurationWhenSetInVisualGridRunner() {
+        ProxySettings proxySettings = new ProxySettings("http://127.0.0.1", 8080);
+        VisualGridRunner visualGridRunner = new VisualGridRunner();
+        visualGridRunner.setProxy(proxySettings); // should set both proxy and autProxy
+
+        Assert.assertEquals(visualGridRunner.getProxy(), proxySettings);
+        Assert.assertNotNull(visualGridRunner.getAutProxy());
+
+        Eyes eyes = new Eyes(visualGridRunner);
+
+        Configuration configuration = eyes.getConfiguration();
+        ProxySettings proxySettings2 = new ProxySettings("http://127.0.0.2", 8080);
+        AutProxySettings autProxySettings = configuration.getAutProxy();
+        AutProxySettings autProxySettings2 = new AutProxySettings(proxySettings2);
+
+        configuration.setAutProxy(autProxySettings2); // should change to second set autProxy
+        eyes.setConfiguration(configuration);
+
+        Assert.assertEquals(eyes.getConfiguration().getAutProxy(), autProxySettings2);
+        Assert.assertNotEquals(autProxySettings, autProxySettings2);
     }
 }
--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestAutProxy.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestAutProxy.java
@@ -4,7 +4,8 @@ import com.applitools.eyes.AutProxyMode;
 import com.applitools.eyes.AutProxySettings;
 import com.applitools.eyes.EyesException;
 import com.applitools.eyes.ProxySettings;
-import com.applitools.eyes.config.Configuration;
+import com.applitools.eyes.exceptions.DiffsFoundException;
+import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.utils.SeleniumUtils;
 import com.applitools.eyes.visualgrid.services.RunnerOptions;
 import com.applitools.eyes.visualgrid.services.VisualGridRunner;
@@ -12,10 +13,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.io.IOException;
+
 public class TestAutProxy {
 
     private WebDriver driver;
-    private VisualGridRunner runner;
 
     @BeforeTest
     public void setup() {
@@ -32,149 +34,88 @@ public class TestAutProxy {
             driver.quit();
     }
 
-    @Test
-    private void shouldSucceedWithAutProxy() {
-        runner = new VisualGridRunner();
-        Eyes eyes = new Eyes(runner);
+    private void startProxyDocker() throws IOException, InterruptedException {
+        Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker run -d --name='tinyproxy' -p 8080:8888 dannydirect/tinyproxy:latest ANY"});
+        stopDocker.waitFor();
+    }
 
-        try {
-
-            Configuration configuration = new Configuration();
-            AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080));
-            configuration.setAutProxy(autProxySettings);
-            eyes.setConfiguration(configuration);
-
-            eyes.open(driver, "AutProxyTest", "autProxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
-        }
+    private void stopAllDockers() throws IOException, InterruptedException {
+        Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker stop $(docker ps -a -q)"});
+        stopDocker.waitFor();
+        Process removeDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker rm $(docker ps -a -q)"});
+        removeDocker.waitFor();
     }
 
     @Test
-    private void shouldSucceedWithAutProxyWithDomains() {
-        runner = new VisualGridRunner();
-        Eyes eyes = new Eyes(runner);
+    public void shouldWorkThenFailWithAutProxy() throws IOException, InterruptedException {
+        networkFailWithProxy();
+        networkAndCheckPassWithAutProxy();
+        CheckFailWithoutAutProxy();
+    }
+
+    private void networkFailWithProxy() throws IOException, InterruptedException {
+        stopAllDockers();
+        WebDriver driver = SeleniumUtils.createChromeDriver();
+        boolean isOpenFailed = false;
+        try {
+            VisualGridRunner visualGridRunner = new VisualGridRunner(new RunnerOptions());
+            Eyes eyes = new Eyes(visualGridRunner);
+
+            eyes.setProxy(new ProxySettings("http://127.0.0.1", 8080));
+            eyes.open(driver, "ProxyTest", "proxy test");
+        } catch (Exception e){
+            isOpenFailed = true;
+        } finally {
+            driver.quit();
+        }
+        Assert.assertTrue(isOpenFailed);
+    }
+
+    private void networkAndCheckPassWithAutProxy() throws IOException, InterruptedException {
+        stopAllDockers();
+        startProxyDocker();
+        WebDriver driver = SeleniumUtils.createChromeDriver();
+        AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080),
+                new String[]{"applitools.github.io", "github.com"}, AutProxyMode.BLOCK);
         try {
 
-            Configuration configuration = new Configuration();
-            AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080),
-                    new String[]{"google.com"});
-            configuration.setAutProxy(autProxySettings);
-            eyes.setConfiguration(configuration);
+            VisualGridRunner visualGridRunner = new VisualGridRunner(new RunnerOptions().autProxy(autProxySettings));
+            Eyes eyes = new Eyes(visualGridRunner);
 
-            driver.get("https://www.google.com");
-            eyes.open(driver, "AutProxyTest", "autProxy with domains test");
+            driver.get("https://applitools.github.io/demo/TestPages/PaddedBody/region-padding.html");
+
+            eyes.open(driver, "AutProxyTest", "aut proxy test");
             Assert.assertTrue(eyes.getIsOpen());
+            eyes.check(Target.window());
             eyes.close();
         } finally {
-            eyes.abortIfNotClosed();
+            driver.quit();
         }
     }
 
-    @Test
-    private void shouldSucceedWithAutProxyWithDomainsAndAutModeAllow() {
-        runner = new VisualGridRunner();
-        Eyes eyes = new Eyes(runner);
+    private void CheckFailWithoutAutProxy() throws IOException, InterruptedException {
+        stopAllDockers();
+        WebDriver driver = SeleniumUtils.createChromeDriver();
+        boolean isFailed = false;
         try {
 
-            Configuration configuration = new Configuration();
-            AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080),
-                    new String[]{"google.com"}, AutProxyMode.ALLOW);
-            configuration.setAutProxy(autProxySettings);
-            eyes.setConfiguration(configuration);
+            VisualGridRunner visualGridRunner = new VisualGridRunner(new RunnerOptions());
+            Eyes eyes = new Eyes(visualGridRunner);
 
-            driver.get("https://www.google.com");
-            eyes.open(driver, "AutProxyTest", "autProxy with domains and allow mode test");
+            driver.get("https://applitools.github.io/demo/TestPages/PaddedBody/region-padding.html");
+
+            eyes.open(driver, "AutProxyTest", "aut proxy test");
             Assert.assertTrue(eyes.getIsOpen());
+            eyes.check(Target.window());
             eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
+        } catch (DiffsFoundException e) {
+            System.out.println(e.getMessage());
+            isFailed = true;
         }
-    }
-
-    @Test
-    private void shouldSucceedWithAutProxyWithDomainsAndAutModeBlock() {
-        runner = new VisualGridRunner();
-        Eyes eyes = new Eyes(runner);
-        try {
-
-            Configuration configuration = new Configuration();
-            AutProxySettings autProxySettings = new AutProxySettings(new ProxySettings("http://127.0.0.1", 8080),
-                    new String[]{"google.com"}, AutProxyMode.BLOCK);
-            configuration.setAutProxy(autProxySettings);
-            eyes.setConfiguration(configuration);
-
-            driver.get("https://www.google.com");
-            eyes.open(driver, "AutProxyTest", "autProxy with domains and block mode test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
+        finally {
+            driver.quit();
         }
-    }
 
-    @Test
-    private void shouldSucceedWithRunnerAutProxy() {
-        runner = new VisualGridRunner();
-        runner.setProxy(new ProxySettings("http://127.0.0.1", 8080));
-        Eyes eyes = new Eyes(runner);
-
-        try {
-
-            eyes.open(driver, "AutProxyTest", "autProxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
-        }
-    }
-
-    @Test
-    private void shouldSucceedWithRunnerAutProxyWithDomains() {
-        RunnerOptions options = new RunnerOptions().autProxy(new ProxySettings("http://127.0.0.1", 8080),
-                new String[]{"google.com"});
-        runner = new VisualGridRunner(options);
-        Eyes eyes = new Eyes(runner);
-        try {
-
-            eyes.open(driver, "AutProxyTest", "autProxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
-        }
-    }
-
-    @Test
-    private void shouldSucceedWithRunnerAutProxyWithDomainsAndAutModeAllow() {
-        RunnerOptions options = new RunnerOptions().autProxy(new ProxySettings("http://127.0.0.1", 8080),
-                new String[]{"google.com"}, AutProxyMode.ALLOW);
-        runner = new VisualGridRunner(options);
-        Eyes eyes = new Eyes(runner);
-        try {
-
-            eyes.open(driver, "AutProxyTest", "autProxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
-        }
-    }
-
-    @Test
-    private void shouldSucceedWithRunnerAutProxyWithDomainsAndAutModeBlock() {
-        RunnerOptions options = new RunnerOptions().autProxy(new ProxySettings("http://127.0.0.1", 8080),
-                new String[]{"google.com"}, AutProxyMode.ALLOW);
-        Eyes eyes = new Eyes(runner);
-        try {
-
-            eyes.open(driver, "AutProxyTest", "autProxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            eyes.abortIfNotClosed();
-        }
+        Assert.assertTrue(isFailed);
     }
 }
