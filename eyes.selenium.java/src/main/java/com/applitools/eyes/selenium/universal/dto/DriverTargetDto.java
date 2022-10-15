6e972e7f9dc83366d95699ecf2fdd5931ed093bb
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-15 17:34:58 +0300

    add DriverTargetDto, ImageTargetDto and ITargetDto

---
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/universal/dto/DriverTargetDto.java
@@ -0,0 +1,45 @@
+package com.applitools.eyes.selenium.universal.dto;
+
+import com.applitools.eyes.AbstractProxySettings;
+
+import java.util.Map;
+
+/**
+ * the driver target
+ */
+public class DriverTargetDto implements ITargetDto {
+
+    private String serverUrl;
+    private AbstractProxySettings proxySettings;
+    private String sessionId;
+    private Map<String, Object> capabilities;
+
+    public void setServerUrl(String serverUrl) { this.serverUrl = serverUrl; }
+
+    public String getServerUrl() { return serverUrl; }
+
+
+    public AbstractProxySettings getProxySettings() {
+        return proxySettings;
+    }
+
+    public void setProxySettings(AbstractProxySettings proxySettings) {
+        this.proxySettings = proxySettings;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Map<String, Object> getCapabilities() {
+        return capabilities;
+    }
+
+    public void setCapabilities(Map<String, Object> capabilities) {
+        this.capabilities = capabilities;
+    }
+}
---
+++ eyes.selenium.java/src/main/java/com/applitools/eyes/selenium/universal/dto/ITargetDto.java
@@ -0,0 +1,4 @@
+package com.applitools.eyes.selenium.universal.dto;
+
+public interface ITargetDto {
+}
