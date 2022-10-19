bb47e2358a426b0b9e00c76d52f27123e073083b
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-19 10:53:44 +0300

    update AndroidTestSetup to have the correct SauceLabs capabilities

--- eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/AndroidTestSetup.java
+++ eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/AndroidTestSetup.java
@@ -2,6 +2,7 @@ package com.applitools.eyes.appium.android;
 
 import com.applitools.eyes.appium.TestSetup;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.MutableCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,18 +11,21 @@ public abstract class AndroidTestSetup extends TestSetup {
 
     @Override
     public void setCapabilities() {
-        super.setCapabilities();
+        capabilities = new MutableCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Google Pixel 2");
-        capabilities.setCapability("os_version", "9.0");
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("newCommandTimeout", 300);
+        capabilities.setCapability("appium:app", "https://applitools.jfrog.io/artifactory/Examples/androidx/helper_lib/1.8.6/app-androidx-debug.apk");
+        capabilities.setCapability("appium:deviceName", "Google Pixel 5 GoogleAPI Emulator");
+        capabilities.setCapability("appium:platformVersion", "11.0");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("appiumVersion", "1.22.1");
+
+        capabilities.setCapability("sauce:options", sauceOptions);
     }
 
     @Override
     protected void initDriver() throws MalformedURLException {
-        driver = new AndroidDriver(new URL(appiumServerUrl), capabilities);
-
+        driver = new AndroidDriver(new URL(SL_URL), capabilities);
     }
 
     @Override
