b10335d47e8420af9f60a6717d79ded48f511d79
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-26 10:41:25 +0300

    add proxy/autProxy tests

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestProxy.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestProxy.java
@@ -1,82 +1,152 @@
 package com.applitools.eyes.selenium;
 
-import com.applitools.connectivity.ServerConnector;
-import com.applitools.eyes.AbstractProxySettings;
+import com.applitools.eyes.AutProxySettings;
 import com.applitools.eyes.ProxySettings;
+import com.applitools.eyes.selenium.fluent.Target;
+import com.applitools.eyes.selenium.universal.dto.DebugEyesDto;
+import com.applitools.eyes.selenium.universal.dto.DebugHistoryDto;
 import com.applitools.eyes.utils.ReportingTestSuite;
 import com.applitools.eyes.utils.SeleniumUtils;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import com.applitools.utils.GeneralUtils;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.openqa.selenium.WebDriver;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-import static org.mockito.Mockito.when;
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(GeneralUtils.class)
 public class TestProxy extends ReportingTestSuite {
 
-    public TestProxy() {
-        super.setGroupName("selenium");
+    private WebDriver driver;
+    private ProxySettings proxySettings;
+    private AutProxySettings autProxySettings;
+
+    @BeforeTest
+    public void setup() {
+        driver = SeleniumUtils.createChromeDriver(new ChromeOptions().setHeadless(true));
+        proxySettings = new ProxySettings("http://127.0.0.1", 8080);
+        autProxySettings = new AutProxySettings(proxySettings);
     }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+// this test is passing when running it alone.
+// if the test runs in the suite - the universal will start without debug mode and
+// rest of the tests would fail.
+//    @Test
+//    public void shouldBeNullWhenDebugIsOff() {
+//        VisualGridRunner runner = new VisualGridRunner();
+//        runner.getAllTestResults(false);
+//        Assert.assertNull(CommandExecutor.getDebugHistory());
+//    }
 
     @Test
-    public void testNetworkThroughProxy() throws IOException, InterruptedException {
-        checkNetworkPassThroughProxy();
-        checkNetworkFailIfNoProxy();
-    }
+    public void testUniversalProxyWithVisualGridRunner() {
 
-    private void checkNetworkFailIfNoProxy() throws IOException, InterruptedException{
-        stopAllDockers();
-        WebDriver driver2 = SeleniumUtils.createChromeDriver();
-        boolean isOpenFailed = false;
-        try {
-            Eyes eyes = new Eyes();
-            eyes.setProxy(new ProxySettings("http://127.0.0.1", 8080));
-            eyes.open(driver2, "ProxyTest", "proxy test");
-        } catch (Exception e){
-            isOpenFailed = true;
-        } finally {
-            driver2.quit();
-        }
-        Assert.assertTrue(isOpenFailed);
-    }
+        try (MockedStatic<GeneralUtils> utilities = Mockito.mockStatic(GeneralUtils.class, Mockito.CALLS_REAL_METHODS)) {
+            utilities.when(() -> GeneralUtils.getEnvString("APPLITOOLS_UNIVERSAL_DEBUG"))
+                    .thenReturn("true");
 
-    private void checkNetworkPassThroughProxy() throws IOException, InterruptedException{
-        stopAllDockers();
-        startProxyDocker();
-        WebDriver driver1 = SeleniumUtils.createChromeDriver();
-        try {
-            Eyes eyes = new Eyes();
-            eyes.setProxy(new ProxySettings("http://127.0.0.1", 8080));
-            eyes.open(driver1, "ProxyTest", "proxy test");
-            Assert.assertTrue(eyes.getIsOpen());
-            eyes.close();
-        } finally {
-            driver1.quit();
+            VisualGridRunner runner = new VisualGridRunner();
+            Eyes eyes = new Eyes(runner);
+
+            eyes.setConfiguration(eyes.getConfiguration()
+                    .setProxy(proxySettings)
+            );
+
+            eyes.open(driver, "ProxyTest", "proxyTestVisualGridRunner");
+            driver.get("https://applitools.com");
+
+            eyes.check(Target.window().fully(false));
+            eyes.close(false);
+
+            runner.getAllTestResults(false);
+
+            DebugHistoryDto history = CommandExecutor.getDebugHistory();
+            DebugEyesDto debugEyes = history.getManagers().get(0).getEyes().get(0);
+
+            Assert.assertNotNull(debugEyes.getConfig());
+            Assert.assertEquals(debugEyes.getConfig().getProxy().getUrl(), proxySettings.getUri());
         }
     }
 
-    @Test
-    public void testProxyNoUrl() {
-        PowerMockito.spy(GeneralUtils.class);
-        when(GeneralUtils.getEnvString(AbstractProxySettings.PROXY_ENV_VAR_NAME)).thenReturn("http://localhost:8888");
-        ServerConnector serverConnector = new ServerConnector();
-        serverConnector.setProxy(new ProxySettings());
-        Assert.assertEquals(serverConnector.getProxy().getUri(), "http://localhost:8888");
+    @Test(dependsOnMethods = {"testUniversalProxyWithVisualGridRunner"})
+    public void testUniversalProxyWithClassicRunner() {
+
+        try (MockedStatic<GeneralUtils> utilities = Mockito.mockStatic(GeneralUtils.class, Mockito.CALLS_REAL_METHODS)) {
+            utilities.when(() -> GeneralUtils.getEnvString("APPLITOOLS_UNIVERSAL_DEBUG"))
+                    .thenReturn("true");
+
+            ClassicRunner runner = new ClassicRunner();
+            Eyes eyes = new Eyes(runner);
+
+            eyes.setConfiguration(eyes.getConfiguration()
+                    .setProxy(proxySettings)
+            );
+
+            eyes.open(driver, "ProxyTest", "proxyTestClassicRunner");
+            driver.get("https://applitools.com");
+
+            eyes.check(Target.window().fully(false));
+            eyes.close(false);
+
+            runner.getAllTestResults(false);
+
+            DebugHistoryDto history = CommandExecutor.getDebugHistory();
+            DebugEyesDto debugEyes = history.getManagers().get(1).getEyes().get(0);
+
+            Assert.assertNotNull(debugEyes.getConfig());
+            Assert.assertEquals(debugEyes.getConfig().getProxy().getUrl(), proxySettings.getUri());
+        }
     }
 
+    @Test(dependsOnMethods = {"testUniversalProxyWithClassicRunner"})
+    public void testUniversalAutProxyWithVisualGridRunner() {
+
+        try (MockedStatic<GeneralUtils> utilities = Mockito.mockStatic(GeneralUtils.class, Mockito.CALLS_REAL_METHODS)) {
+            utilities.when(() -> GeneralUtils.getEnvString("APPLITOOLS_UNIVERSAL_DEBUG"))
+                    .thenReturn("true");
+
+            VisualGridRunner runner = new VisualGridRunner();
+            Eyes eyes = new Eyes(runner);
+
+            eyes.setConfiguration(eyes.getConfiguration()
+                    .setAutProxy(autProxySettings)
+            );
+
+            eyes.open(driver, "ProxyTest", "autProxyTestVisualGridRunner");
+            driver.get("https://applitools.com");
+
+            eyes.check(Target.window().fully(false));
+            eyes.close(false);
+
+            runner.getAllTestResults(false);
+
+            DebugHistoryDto history = CommandExecutor.getDebugHistory();
+            DebugEyesDto debugEyes = history.getManagers().get(2).getEyes().get(0);
+
+            Assert.assertNotNull(debugEyes.getConfig());
+            Assert.assertNull(debugEyes.getConfig().getProxy());
+            Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUrl(), autProxySettings.getUri());
+        }
+    }
+
+    // not in use
     private void startProxyDocker() throws IOException, InterruptedException {
         Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker run -d --name='tinyproxy' -p 8080:8888 dannydirect/tinyproxy:latest ANY"});
         stopDocker.waitFor();
     }
 
+    // not in use
     private void stopAllDockers() throws IOException, InterruptedException {
         Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker stop $(docker ps -a -q)"});
         stopDocker.waitFor();
@@ -84,3 +154,4 @@ public class TestProxy extends ReportingTestSuite {
         removeDocker.waitFor();
     }
 }
+
