7604d552adeef2510994f9f06e6c6ef5803419a8
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-27 13:06:13 +0300

    update TestProxy - add test cases

--- eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestProxy.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/selenium/TestProxy.java
@@ -1,9 +1,6 @@
 package com.applitools.eyes.selenium;
 
-import com.applitools.eyes.AutProxySettings;
-import com.applitools.eyes.EyesException;
-import com.applitools.eyes.ProxySettings;
-import com.applitools.eyes.WebDriverProxySettings;
+import com.applitools.eyes.*;
 import com.applitools.eyes.selenium.universal.dto.DebugEyesDto;
 import com.applitools.eyes.selenium.universal.dto.DebugHistoryDto;
 import com.applitools.eyes.selenium.universal.server.UniversalSdkNativeLoader;
@@ -24,7 +21,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestProxy extends ReportingTestSuite {
@@ -33,6 +29,8 @@ public class TestProxy extends ReportingTestSuite {
     private WebDriver driver;
     private ProxySettings proxySettings;
     private AutProxySettings autProxySettings;
+    private ProxySettings proxySettings1;
+    private AutProxySettings autProxySettings1;
 
     private Method stopServer;
     private Field instance;
@@ -48,6 +46,9 @@ public class TestProxy extends ReportingTestSuite {
         proxySettings = new ProxySettings("http://127.0.0.1", 8080, "username", "password");
         autProxySettings = new AutProxySettings(proxySettings);
 
+        proxySettings1 = new ProxySettings("http://127.0.0.1", 8081);
+        autProxySettings1 = new AutProxySettings(proxySettings1, new String[]{"http://domain.com"}, AutProxyMode.ALLOW);
+
         stopServer = UniversalSdkNativeLoader.class.getDeclaredMethod("destroyProcess");
         stopServer.setAccessible(true);
 
@@ -59,9 +60,6 @@ public class TestProxy extends ReportingTestSuite {
         Field modifiersField = Field.class.getDeclaredField("modifiers");
         modifiersField.setAccessible(true);
         modifiersField.setInt(debug, debug.getModifiers() & ~Modifier.FINAL);
-
-        stopAllDockers();
-        startProxyDocker();
     }
 
     @AfterTest
@@ -80,6 +78,7 @@ public class TestProxy extends ReportingTestSuite {
     public void beforeEach() {
         try {
             instance.set(this, null);
+
         } catch (IllegalAccessException e) {
             String errorMessage = GeneralUtils.createErrorMessageFromExceptionWithText(e, "Failed to destroy process / reset CE instance");
             System.out.println(errorMessage);
@@ -103,13 +102,14 @@ public class TestProxy extends ReportingTestSuite {
     }
 
     @Test
-    public void shouldBeNullWhenDebugIsOff() {
+    public void debugHistoryShouldBeNullWhenDebugIsOff() {
         new VisualGridRunner();
         Assert.assertNull(CommandExecutor.getDebugHistory());
     }
 
     @Test
-    public void testUniversalProxyWithVisualGridRunner() throws IllegalAccessException {
+    public void testUniversalProxyWithVisualGridRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8080);
         debug.set(this, "true");
 
         VisualGridRunner runner = new VisualGridRunner();
@@ -131,7 +131,8 @@ public class TestProxy extends ReportingTestSuite {
     }
 
     @Test
-    public void testUniversalProxyWithClassicRunner() throws IllegalAccessException {
+    public void testUniversalProxyWithClassicRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8080);
         debug.set(this, "true");
 
         ClassicRunner runner = new ClassicRunner();
@@ -176,7 +177,33 @@ public class TestProxy extends ReportingTestSuite {
     }
 
     @Test
-    public void testUniversalProxyAndAutProxyWithVisualGridRunner() throws IllegalAccessException {
+    public void testUniversalAutProxyWithClassicRunner() throws IllegalAccessException {
+        debug.set(this, "true");
+
+        ClassicRunner runner = new ClassicRunner();
+        eyes = new Eyes(runner);
+
+        eyes.setConfiguration(eyes.getConfiguration()
+                .setAutProxy(autProxySettings1)
+        );
+
+        eyes.open(driver, "ProxyTest", "autProxyTestClassicRunner");
+
+        DebugHistoryDto history = CommandExecutor.getDebugHistory();
+        DebugEyesDto debugEyes = history.getManagers().get(0).getEyes().get(0);
+
+        Assert.assertNotNull(debugEyes.getConfig());
+        Assert.assertNull(debugEyes.getConfig().getProxy());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUrl(), autProxySettings1.getUri());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUsername(), autProxySettings1.getUsername());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getPassword(), autProxySettings1.getPassword());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getDomains(), autProxySettings1.getDomains());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getAutProxyMode(), autProxySettings1.getAutProxyMode().getName());
+    }
+
+    @Test
+    public void testUniversalProxyAndAutProxyWithVisualGridRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8080);
         debug.set(this, "true");
 
         VisualGridRunner runner = new VisualGridRunner();
@@ -194,15 +221,48 @@ public class TestProxy extends ReportingTestSuite {
 
         Assert.assertNotNull(debugEyes.getConfig());
         Assert.assertEquals(debugEyes.getConfig().getProxy().getUrl(), proxySettings.getUri());
-        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUrl(), autProxySettings.getUri());
         Assert.assertEquals(debugEyes.getConfig().getProxy().getUsername(), proxySettings.getUsername());
         Assert.assertEquals(debugEyes.getConfig().getProxy().getPassword(), proxySettings.getPassword());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUrl(), autProxySettings.getUri());
         Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUsername(), autProxySettings.getUsername());
         Assert.assertEquals(debugEyes.getConfig().getAutProxy().getPassword(), autProxySettings.getPassword());
     }
 
     @Test
-    public void testUniversalWebDriverProxyWithVisualGridRunner() throws IllegalAccessException, MalformedURLException {
+    public void testUniversalProxyWithDifferentAutProxyWithClassicRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8081);
+        debug.set(this, "true");
+
+        // change aut mode from other tests
+        autProxySettings1 = new AutProxySettings(proxySettings1, new String[]{"http://domain.com"}, AutProxyMode.BLOCK);
+
+        VisualGridRunner runner = new VisualGridRunner();
+        eyes = new Eyes(runner);
+
+        eyes.setConfiguration(eyes.getConfiguration()
+                .setProxy(proxySettings1)
+                .setAutProxy(autProxySettings1)
+        );
+
+        eyes.open(driver, "ProxyTest", "proxyAndDifferentAutProxyTestClassicRunner");
+
+        DebugHistoryDto history = CommandExecutor.getDebugHistory();
+        DebugEyesDto debugEyes = history.getManagers().get(0).getEyes().get(0);
+
+        Assert.assertNotNull(debugEyes.getConfig());
+        Assert.assertEquals(debugEyes.getConfig().getProxy().getUrl(), proxySettings1.getUri());
+        Assert.assertEquals(debugEyes.getConfig().getProxy().getUsername(), proxySettings1.getUsername());
+        Assert.assertEquals(debugEyes.getConfig().getProxy().getPassword(), proxySettings1.getPassword());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUrl(), autProxySettings1.getUri());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getUsername(), autProxySettings1.getUsername());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getPassword(), autProxySettings1.getPassword());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getDomains(), autProxySettings1.getDomains());
+        Assert.assertEquals(debugEyes.getConfig().getAutProxy().getAutProxyMode(), autProxySettings1.getAutProxyMode().getName());
+    }
+
+    @Test
+    public void testUniversalWebDriverProxyWithVisualGridRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8080);
         debug.set(this, "true");
 
         final String USERNAME = GeneralUtils.getEnvString("SAUCE_USERNAME");
@@ -219,7 +279,7 @@ public class TestProxy extends ReportingTestSuite {
                 .setWebDriverProxy(new WebDriverProxySettings("http://127.0.0.1:8080"))
         );
 
-        eyes.open(driver, "ProxyTest", "autProxyTestVisualGridRunner");
+        eyes.open(driver, "ProxyTest", "webdriverProxyVisualGridRunner");
         driver.quit();
 
         DebugHistoryDto history = CommandExecutor.getDebugHistory();
@@ -231,8 +291,44 @@ public class TestProxy extends ReportingTestSuite {
         Assert.assertEquals(debugEyes.getDriver().getProxyUrl(), "http://127.0.0.1:8080");
     }
 
-    private void startProxyDocker() throws IOException, InterruptedException {
-        Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker run -d --name='tinyproxy' -p 8080:8888 dannydirect/tinyproxy:latest ANY"});
+    @Test
+    public void testUniversalWebDriverProxyWithClassicRunner() throws IllegalAccessException, IOException, InterruptedException {
+        stopAndStartDockers(8081);
+        debug.set(this, "true");
+
+        final String USERNAME = GeneralUtils.getEnvString("SAUCE_USERNAME");
+        final String ACCESS_KEY = GeneralUtils.getEnvString("SAUCE_ACCESS_KEY");
+        final String SL_URL = "https://"+USERNAME+":" + ACCESS_KEY + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities("chrome", "", Platform.ANY);
+
+        WebDriver driver = new RemoteWebDriver(new URL(SL_URL), caps);
+        ClassicRunner runner = new ClassicRunner();
+        eyes = new Eyes(runner);
+
+        eyes.setConfiguration(eyes.getConfiguration()
+                .setWebDriverProxy(new WebDriverProxySettings("http://127.0.0.1:8081"))
+        );
+
+        eyes.open(driver, "ProxyTest", "webdriverProxyClassicRunner");
+        driver.quit();
+
+        DebugHistoryDto history = CommandExecutor.getDebugHistory();
+        DebugEyesDto debugEyes = history.getManagers().get(0).getEyes().get(0);
+
+        Assert.assertNotNull(debugEyes.getConfig());
+        Assert.assertNull(debugEyes.getConfig().getProxy());
+        Assert.assertNull(debugEyes.getConfig().getAutProxy());
+        Assert.assertEquals(debugEyes.getDriver().getProxyUrl(), "http://127.0.0.1:8081");
+    }
+
+    private void stopAndStartDockers(int port) throws IOException, InterruptedException {
+        stopAllDockers();
+        startProxyDocker(port);
+    }
+
+    private void startProxyDocker(int port) throws IOException, InterruptedException {
+        Process stopDocker = Runtime.getRuntime().exec(new String[]{"bash","-c","docker run -d --name='tinyproxy' -p "+port+":8888 dannydirect/tinyproxy:latest ANY"});
         stopDocker.waitFor();
     }
 
