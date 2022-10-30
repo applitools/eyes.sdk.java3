41eada9620de6237a34c261b058a4169805e31b3
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-30 11:23:24 +0200

    add Target.image with url

--- eyes.images.java/src/main/java/com/applitools/eyes/images/Target.java
+++ eyes.images.java/src/main/java/com/applitools/eyes/images/Target.java
@@ -2,8 +2,10 @@ package com.applitools.eyes.images;
 
 import com.applitools.ICheckSettings;
 import com.applitools.utils.ImageUtils;
+import sun.jvm.hotspot.utilities.BitMap;
 
 import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public class Target {
 
@@ -15,5 +17,16 @@ public class Target {
         return image(ImageUtils.imageFromFile(path));
     }
 
-    public static ICheckSettings image(byte[] imageBytes) { return image(ImageUtils.imageFromBytes(imageBytes)); }
+    public static ICheckSettings image(byte[] imageBytes) {
+        return image(ImageUtils.imageFromBytes(imageBytes));
+    }
+
+//    // TODO
+//    public static ICheckSettings image(BitMap image) {
+//        return null;
+//    }
+
+    public static ICheckSettings image(URL url) {
+        return image(ImageUtils.imageFromUrl(url));
+    }
 }
