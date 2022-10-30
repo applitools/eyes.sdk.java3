255c9e2a76abed0fde460c2962541eda1f24a666
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-30 11:41:07 +0200

    remove BitMap

--- eyes.images.java/src/main/java/com/applitools/eyes/images/Target.java
+++ eyes.images.java/src/main/java/com/applitools/eyes/images/Target.java
@@ -2,7 +2,6 @@ package com.applitools.eyes.images;
 
 import com.applitools.ICheckSettings;
 import com.applitools.utils.ImageUtils;
-import sun.jvm.hotspot.utilities.BitMap;
 
 import java.awt.image.BufferedImage;
 import java.net.URL;
@@ -20,11 +19,6 @@ public class Target {
     public static ICheckSettings image(byte[] imageBytes) {
         return image(ImageUtils.imageFromBytes(imageBytes));
     }
-
-//    // TODO
-//    public static ICheckSettings image(BitMap image) {
-//        return null;
-//    }
 
     public static ICheckSettings image(URL url) {
         return image(ImageUtils.imageFromUrl(url));
