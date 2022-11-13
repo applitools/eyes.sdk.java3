332f0e4c47358e0c4fad95832c049e314e8c35b8
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-13 18:31:34 +0200

    change coverage-tests configuration to a branch that skips lazyLoad tests

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,12 +1,12 @@
 module.exports = {
   name: "eyes_selenium_java",
-  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/emitter.js",
+  emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_jpmc_skip_lazyload/java/emitter.js",
   overrides: [
-    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js",
-    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/overrides.js"
+    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_jpmc_skip_lazyload/js/overrides.js",
+    "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_jpmc_skip_lazyload/java/overrides.js"
   ],
-  template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/java/template.hbs",
-  tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js",
+  template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_jpmc_skip_lazyload/java/template.hbs",
+  tests: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/java_jpmc_skip_lazyload/coverage-tests.js",
   ext: ".java",
   outPath: "./src/test/java/coverage/generic",
 };
