1a4275c359a79611af1e81a65ab7ec42d96d8566
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-15 21:15:14 +0200

    update WrapperWebdriverTests test structure

--- selenium4/src/test/java/com/applitools/selenium4/WrapperWebdriverTests.java
+++ selenium4/src/test/java/com/applitools/selenium4/WrapperWebdriverTests.java
@@ -5,23 +5,37 @@ import org.openqa.selenium.ImmutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class WrapperWebdriverTests {
 
-    @Test
-    public void should_OpenEyes_WhenWrappedDriver() throws Exception {
-        Eyes eyes = new Eyes();
+    private WebDriver driver;
+    private Eyes eyes;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        eyes = new Eyes();
         String url = "https://applitools:zBo67o7BsoKhdkf8Va4u@hub-cloud.browserstack.com/wd/hub";
         ImmutableCapabilities capabilities = new ImmutableCapabilities("browserName", "chrome");
 
-        WebDriver driver = new RemoteWebDriver(new URL(url), capabilities);
+        driver = new RemoteWebDriver(new URL(url), capabilities);
         driver = new Augmenter().augment(driver);
-        eyes.open(driver, "appName", "testName");
-        eyes.close(false);
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void teardown() {
         if (driver != null) driver.quit();
         eyes.abort();
+    }
+
+    @Test
+    public void should_OpenEyes_WhenWrappedDriver() {
+        eyes.open(driver, "appName", "testName");
+        eyes.close(false);
     }
 }
