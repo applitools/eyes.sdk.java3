d0a432e463f08d75981a943f4e449f9423f58ccc
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-21 15:29:57 +0200

    use app url instead of storage link

--- eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/TestAndroidContentSizeFallback.java
+++ eyes.appium.java/src/test/java/com/applitools/eyes/appium/android/TestAndroidContentSizeFallback.java
@@ -7,31 +7,31 @@ import org.testng.annotations.Test;
 
 public class TestAndroidContentSizeFallback extends AndroidTestSetup {
 
-//    @Override
-//    public void setCapabilities() {
-//        System.out.println("setCapabilities shouldNotFailOnTypeErrorWhenNoHelperLib");
-//        capabilities.setCapability("platformName", "Android");
-//        capabilities.setCapability("appium:platformVersion","10.0");
-//        capabilities.setCapability("appium:deviceName","Google Pixel 3a XL GoogleAPI Emulator");
-//        capabilities.setCapability("appium:automationName", "UiAutomator2");
-//        capabilities.setCapability("appium:newCommandTimeout", 2000);
-//
-//        MutableCapabilities sauceOptions = new MutableCapabilities();
-//        sauceOptions.setCapability("appiumVersion", "1.22.1");
-//        sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
-//        sauceOptions.setCapability("name", "Java - Android");
-//        sauceOptions.setCapability("idleTimeout", 300);
-//        capabilities.setCapability("sauce:options", sauceOptions);
-//
-//        setAppCapability();
-//    }
+    @Override
+    public void setCapabilities() {
+        System.out.println("setCapabilities shouldNotFailOnTypeErrorWhenNoHelperLib");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appium:platformVersion","10.0");
+        capabilities.setCapability("appium:deviceName","Google Pixel 3a XL GoogleAPI Emulator");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appium:newCommandTimeout", 2000);
 
-//    @Override
-//    protected void setAppCapability() {
-//        System.out.println("setAppCapability shouldNotFailOnTypeErrorWhenNoHelperLib");
-//        // app-androidx-debug_v4.16.2_no_helper.apk from Sauce storage
-//        capabilities.setCapability("appium:app", "storage:filename=app-androidx-debug_v4.16.2_no_helper.apk");
-//    }
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("appiumVersion", "1.22.1");
+        sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
+        sauceOptions.setCapability("name", "Java - Android");
+        sauceOptions.setCapability("idleTimeout", 300);
+        capabilities.setCapability("sauce:options", sauceOptions);
+
+        setAppCapability();
+    }
+
+    @Override
+    protected void setAppCapability() {
+        System.out.println("setAppCapability shouldNotFailOnTypeErrorWhenNoHelperLib");
+        // app-androidx-debug_v4.16.2_no_helper.apk from Sauce storage
+        capabilities.setCapability("appium:app", "https://applitools.jfrog.io/artifactory/Examples/androidx/no_helper_lib/4.16.2/app.apk");
+    }
 
     @Test
     public void shouldNotFailOnTypeErrorWhenNoHelperLib() throws InterruptedException {
