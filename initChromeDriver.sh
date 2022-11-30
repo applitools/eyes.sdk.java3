2fecd21192e413a378122d72a6c6c264339649bd
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-30 17:23:46 +0200

    set chromedriver version to 107.0.5304.62 in initChromeDriver.sh

--- initChromeDriver.sh
+++ initChromeDriver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 chromeVersion=`google-chrome --product-version | sed 's/\..*//'`
 echo $chromeVersion
-latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromeVersion -q -O -)
+latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_107.0.5304.62 -q -O -)
 #latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_99 -q -O -)
 echo $latestChromeDriverURL
 wget "http://chromedriver.storage.googleapis.com/${latestChromeDriverURL}/chromedriver_linux64.zip"
