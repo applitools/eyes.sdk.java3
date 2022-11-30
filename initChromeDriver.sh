d2555c118d99a741de40e8bd028d9636a37c2ca8
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-30 17:35:59 +0200

    hard code chromedriver version as 107.0.5304.62

--- initChromeDriver.sh
+++ initChromeDriver.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 chromeVersion=`google-chrome --product-version | sed 's/\..*//'`
 echo $chromeVersion
-latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_107.0.5304.62 -q -O -)
+latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromeVersion -q -O -)
 #latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_99 -q -O -)
 echo $latestChromeDriverURL
-wget "http://chromedriver.storage.googleapis.com/${latestChromeDriverURL}/chromedriver_linux64.zip"
+wget "http://chromedriver.storage.googleapis.com/107.0.5304.62/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip -d /home/travis/build/
 chmod +x /home/travis/build/chromedriver
 /home/travis/build/chromedriver --version
