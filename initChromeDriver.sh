4a9ba5edfca1c64c1af1c3e01388e7224d25dc1d
IdosApplitools <ido.elmaleh@applitools.com>
2022-11-30 19:43:49 +0200

    revert change in initChromeDriver.sh

--- initChromeDriver.sh
+++ initChromeDriver.sh
@@ -3,7 +3,7 @@ chromeVersion=`google-chrome --product-version | sed 's/\..*//'`
 latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromeVersion -q -O -)
 #latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_99 -q -O -)
 echo $latestChromeDriverURL
-wget "http://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip"
+wget "http://chromedriver.storage.googleapis.com/${latestChromeDriverURL}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip -d /home/travis/build/
 chmod +x /home/travis/build/chromedriver
 /home/travis/build/chromedriver --version
