d5313b3868fb739398145299f9f323ab86879f63
IdosApplitools <ido.elmaleh@applitools.com>
2022-12-27 17:13:52 +0200

    update DriverMapper

--- eyes.playwright.java/src/main/java/com/applitools/eyes/playwright/universal/mapper/DriverMapper.java
+++ eyes.playwright.java/src/main/java/com/applitools/eyes/playwright/universal/mapper/DriverMapper.java
@@ -5,7 +5,16 @@ import com.applitools.eyes.playwright.universal.dto.DriverTargetDto;
 import com.microsoft.playwright.Page;
 
 public class DriverMapper {
-    public static DriverTargetDto toDriverTargetDto(Page page, WebDriverProxySettings webDriverProxy) {
-        return null;
+    public static DriverTargetDto toDriverTargetDto(Page page, WebDriverProxySettings wdProxy) {
+        if (page == null) {
+            return null;
+        }
+
+        DriverTargetDto driverTargetDto = new DriverTargetDto();
+//        driverTargetDto.setSessionId(remoteDriver.getSessionId().toString());
+        driverTargetDto.setServerUrl(page.url());
+//        driverTargetDto.setCapabilities(remoteDriver.getCapabilities().asMap());
+
+        return driverTargetDto;
     }
 }
