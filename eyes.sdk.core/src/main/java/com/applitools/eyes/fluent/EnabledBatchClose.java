9420540a9b3a282fa8d38a3112d38d4667876394
IdosApplitools <ido.elmaleh@applitools.com>
2023-01-17 12:25:04 +0200

    update EnabledBatchClose to start a server only if the one is not alive

--- eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/EnabledBatchClose.java
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/fluent/EnabledBatchClose.java
@@ -1,7 +1,10 @@
 package com.applitools.eyes.fluent;
 
+import com.applitools.eyes.EyesException;
+import com.applitools.eyes.EyesRunner;
 import com.applitools.eyes.Logger;
 import com.applitools.eyes.ProxySettings;
+import com.applitools.eyes.exceptions.StaleElementReferenceException;
 import com.applitools.eyes.logging.Stage;
 import com.applitools.eyes.logging.Type;
 import com.applitools.eyes.universal.CommandExecutor;
@@ -9,21 +12,20 @@ import com.applitools.eyes.universal.dto.CloseBatchSettingsDto;
 import com.applitools.eyes.universal.mapper.SettingsMapper;
 import com.applitools.eyes.universal.server.UniversalSdkNativeLoader;
 import com.applitools.utils.ArgumentGuard;
-import com.applitools.utils.ClassVersionGetter;
+import com.applitools.utils.GeneralUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.List;
 
 public class EnabledBatchClose extends BatchClose {
     private List<String> batchIds;
-    private String BASE_AGENT_ID = "eyes.sdk.java";
 
     EnabledBatchClose(Logger logger, String serverUrl, List<String> batchIds) {
         super(logger);
-        UniversalSdkNativeLoader.setLogger(logger);
-        UniversalSdkNativeLoader.start();
-        CommandExecutor.getInstance(BASE_AGENT_ID, ClassVersionGetter.CURRENT_VERSION, null);
         this.serverUrl = serverUrl;
         this.batchIds = batchIds;
     }
@@ -57,6 +59,10 @@ public class EnabledBatchClose extends BatchClose {
         return this;
     }
 
+    /**
+     * close an open batch.
+     * This
+     */
     public void close() {
         logger.log(new HashSet<String>(), Stage.CLOSE, Type.CLOSE_BATCH, Pair.of("batchSize", batchIds.size()));
 
@@ -64,7 +70,54 @@ public class EnabledBatchClose extends BatchClose {
             return;
         }
 
+        Boolean shouldStopServer = startServer();
         List<CloseBatchSettingsDto> dto = SettingsMapper.toCloseBatchSettingsDto(batchIds, apiKey, serverUrl, proxySettings);
         CommandExecutor.closeBatch(dto);
+        stopServer(shouldStopServer);
+    }
+
+    private Boolean startServer() {
+        boolean shouldStopServer = true;
+
+        // check if server is alive
+        try {
+            Field nativeProcess = UniversalSdkNativeLoader.class.getDeclaredField("nativeProcess");
+            nativeProcess.setAccessible(true);
+            Object isNativeProcess = nativeProcess.get(UniversalSdkNativeLoader.class);
+            if (isNativeProcess != null) {
+                shouldStopServer = false;
+            }
+            nativeProcess.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        // start the server only if it's not alive to avoid runner conflicts
+        if (shouldStopServer) {
+            new EyesRunner() {
+                @Override
+                public StaleElementReferenceException getStaleElementException() {
+                    return null;
+                }
+            };
+        }
+
+
+        return shouldStopServer;
+    }
+
+    private void stopServer(Boolean shouldStopServer) {
+        if (shouldStopServer) {
+            try {
+                Method stopServer = UniversalSdkNativeLoader.class.getDeclaredMethod("destroyProcess");
+                stopServer.setAccessible(true);
+                stopServer.invoke(UniversalSdkNativeLoader.class);
+                stopServer.setAccessible(false);
+            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                String errorMessage = GeneralUtils.createErrorMessageFromExceptionWithText(e, "Failed to stop server!");
+                System.out.println(errorMessage);
+                throw new EyesException(errorMessage, e);
+            }
+        }
     }
 }
