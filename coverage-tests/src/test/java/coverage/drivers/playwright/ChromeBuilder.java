b1c090de48804be43b4a3710d470d30d3ee15838
IdosApplitools <ido.elmaleh@applitools.com>
2023-01-18 16:09:34 +0200

    update playwright generic test driver builder to not set default args for headless mode

--- coverage-tests/src/test/java/coverage/drivers/playwright/ChromeBuilder.java
+++ coverage-tests/src/test/java/coverage/drivers/playwright/ChromeBuilder.java
@@ -6,11 +6,15 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 public class ChromeBuilder implements PlaywrightBuilder {
     @Override
     public Page build(Playwright playwright, boolean headless, boolean legacy, boolean executionGrid) throws MalformedURLException {
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(headless));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(headless)
+                .setIgnoreDefaultArgs(headless ? Arrays.asList("--hide-scrollbars") : null)
+        );
         return browser.newPage();
     }
 }
