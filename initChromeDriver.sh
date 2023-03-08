2de479481b817df6b4c930950ff5e9145290f7df
IdosApplitools <ido.elmaleh@applitools.com>
2023-03-08 15:14:31 +0200

    hardcode chromedriver v110 for Travis tests

--- initChromeDriver.sh
+++ initChromeDriver.sh
@@ -2,6 +2,7 @@
 chromeVersion=`google-chrome --product-version | sed 's/\..*//'`
 latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chromeVersion -q -O -)
 #latestChromeDriverURL=$(wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE_99 -q -O -)
+latestChromeDriverURL="110.0.5481.77"
 echo $latestChromeDriverURL
 wget "http://chromedriver.storage.googleapis.com/${latestChromeDriverURL}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip -d /home/travis/build/
