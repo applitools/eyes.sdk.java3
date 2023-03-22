8e76cb5ed55fde27d81d8711172f7aeab771d0fb
IdosApplitools <ido.elmaleh@applitools.com>
2023-03-22 19:50:38 +0200

    update test action and workflow to encapsulate running tests + remove unused legacy CI files

--- upload_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" \
-  -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
-  -F "data={\"url\": \"$1\", \"custom_id\": \"$2\"}"
