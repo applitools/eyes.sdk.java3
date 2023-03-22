b98d7d922f3f9d6704533d039b93937ac6438cf9
IdosApplitools <ido.elmaleh@applitools.com>
2023-03-22 19:50:38 +0200

    update test action and workflow to encapsulate running tests + remove unused legacy CI files

---
+++ eyes.selenium.java/runTests.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Abort on Error
+set -e;
+
+# Setup web drivers
+echo "Chromedriver setup"
+chmod +x ./../initChromeDriverGHA.sh;
+sh ./../initChromeDriverGHA.sh;
+export CHROME_DRIVER_PATH="/usr/local/bin/chromedriver";
+
+echo "Geckodriver setup"
+chmod +x ./../initGeckoDriverGHA.sh;
+sh ./../initGeckoDriverGHA.sh;
+export FIREFOX_DRIVER_PATH="/usr/local/bin/geckodriver";
+
+# Setup test type
+IFS=' ' read -ra TEST_TYPE_ARRAY <<< "$TEST_TYPE"
+
+function parse_type() {
+  case $1 in
+    unit) echo "unitTestsSuite.xml";;
+    it) echo "integrationTestsSuite.xml";;
+    e2e) echo "e2eTestsSuite.xml";;
+    *) :;;
+  esac
+}
+
+ACTUAL_TEST_TYPE=""
+
+if (( ${#TEST_TYPE_ARRAY[@]} > 1 )); then
+  # the input is an array
+  for value in "${TEST_TYPE_ARRAY[@]}"; do
+    type=$(parse_type "$value")
+    ACTUAL_TEST_TYPE+="$type,"
+  done
+else
+  # the input is a single value
+  type=$(parse_type "$value")
+  ACTUAL_TEST_TYPE+="$type,"
+fi
+
+echo "Test type: $ACTUAL_TEST_TYPE"
+if [[ -z $ACTUAL_TEST_TYPE ]]; then
+  # Run the default suite file
+  mvn test -e -X
+else
+  mvn test -DsuiteFile="$ACTUAL_TEST_TYPE" -e -X
+fi
+
+# Send module report
+if [ -d "$BUILD_DIR/report" ]; then
+  chmod +x ./../sendTestResults.sh;
+  sh ./../sendTestResults.sh;
+else
+  echo "Module report was not created. \"$BUILD_DIR/report\" doesn't exist"
+fi
+
+if [[ "$TEST_TYPE" == *"coverage"* || "$TEST_TYPE" == "all" ]]; then
+  # Run coverage tests
+  cd ../coverage-tests;
+  chmod +x ./generic_tests.sh;
+  sh ./generic_tests.sh true "selenium";
+
+  # Send coverage results
+  if [[ $REPORT_LEVEL == "deploy" ]]; then
+    yarn report:prod-selenium;
+  else
+    yarn report:selenium;
+  fi
+fi
